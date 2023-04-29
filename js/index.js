f249719e0313cc9cecd55b5909878b85028f3c6a
ccczy-czy <zc2122@nyu.edu>
2023-04-29 17:14:00 -0400

    page1 extension

--- js/index.js
+++ js/index.js
@@ -3,6 +3,15 @@ const onresize = (dom_elem, callback) => {
   resizeObserver.observe(dom_elem);
 };
 
+const concatArray = (arr) => {
+  let string = "";
+  for (let i = 0; i < arr.length; i++) {
+    string += arr[i];
+  }
+
+  return string;
+}
+
 const cols = document.querySelectorAll('.col');
 cols.forEach((item) => {
   const oldHeight = parseFloat(getComputedStyle(item).height);
@@ -35,3 +44,41 @@ cols.forEach((item) => {
     item.style.height = item.scrollHeight + 'px';
   });
 });
+
+document.addEventListener("mouseup", function() {
+  const selectedNode = window.getSelection();
+  const selectedText = selectedNode.toString().trim();
+  if (selectedText !== "") {
+    const newInsert = document.createElement('span');
+    newInsert.className += 'selectedText specialCharacter';
+    // newInsert.textContent = selectedText;
+    newInsert.textContent = '*';
+    newInsert.style.maxWidth = 'fit-content';
+    newInsert.style.fontSize = document.querySelector('#slider').value;
+    // newInsert.className += 'specialCharacter';
+
+    /*
+    newInsert.textContent = '*';
+    newInsert.className += 'specialCharacter';
+
+    TODO: 
+    1. Add a slider to control the font size of the *
+    */
+
+    const insertArray = selectedNode.focusNode.parentNode.innerHTML.toString().split(selectedText);
+    insertArray.splice(1, 0, newInsert.outerHTML);
+
+    selectedNode.focusNode.parentNode.innerHTML = concatArray(insertArray);
+
+  }
+});
+
+const slider = document.querySelector('#slider');
+slider.addEventListener("input", (evt) => {
+  const stars = document.querySelectorAll('.specialCharacter');
+  stars.forEach((s) => {
+    s.style.fontSize = evt.target.value + 'rem';
+  });
+});
+
+// console.log(document.querySelector('#slider').value);
